e994258598537ec2d0b4d14a7b469bc4b672f0af
Charles Yang <yang657850144@gmail.com>
2019-01-30 10:48:51 +0800

    fix: modify project construction

--- alias.js
+++
@@ -1,7 +0,0 @@
-const path = require('path')
-
-const resolve = p => path.resolve(__dirname, './', p)
-
-module.exports = {
-  lib: resolve('lib/vue-message')
-}
--- pub.js
+++ pub.js
@@ -1,24 +1,24 @@
-const aliases = require('./alias')
+// const aliases = require('./alias')
 const shell = require('shelljs')
-const path = require('path') // eslint-disable-line
+// const path = require('path') // eslint-disable-line
 // const fs = require('fs')
 
 // copy README to lib
-const resolve = p => {
-  const base = p.split('/')[0]
-  if (aliases[base]) {
-    return path.resolve(aliases[base], p.slice(base.length + 1))
-  } else {
-    return path.resolve(__dirname, './', p)
-  }
-}
+// const resolve = p => {
+//   const base = p.split('/')[0]
+//   if (aliases[base]) {
+//     return path.resolve(aliases[base], p.slice(base.length + 1))
+//   } else {
+//     return path.resolve(__dirname, './', p)
+//   }
+// }
 
-shell.cp('-R', resolve('./README.md'), resolve('lib/README.md'))
-shell.cp('-R', resolve('./README-ZH.md'), resolve('lib/README-ZH.md'))
+// shell.cp('-R', resolve('./README.md'), resolve('lib/README.md'))
+// shell.cp('-R', resolve('./README-ZH.md'), resolve('lib/README-ZH.md'))
 
-setTimeout(_ => {
-  shell.cd('./lib/vue-message')
-  shell.exec('npm run release && npm publish --access=public')
-  shell.cd('../../')
-  shell.exec('git commit -am \'UPDATE README.md\' && git push')
-}, 300)
+shell.exec('npm run release && npm publish --access=public')
+shell.exec('git commit -am \'UPDATE README.md\' && git push')
+// setTimeout(_ => {
+//   // shell.cd('./lib/vue-message')
+//   // shell.cd('../../')
+// }, 300)
